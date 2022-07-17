d81aeab9830814e0b91d3a545163b9e4fdbeb576
katebienko <108893252+katebienko@users.noreply.github.com>
2022-07-17 22:59:34 +0200

    Fix bugs

--- weatherApp/weatherApp/ViewController.swift
+++ weatherApp/weatherApp/ViewController.swift
@@ -1,6 +1,5 @@
 import UIKit
 import CoreLocation
-import FirebaseAnalytics
 
 class ViewController: UIViewController {
     
@@ -28,9 +27,6 @@ class ViewController: UIViewController {
         for name in countryNames {
             let newString = name.replacingOccurrences(of: " ", with: "%20")
             addCity(name: newString)
-            
-            //Analytics Event
-            //Analytics.logEvent("Init Screen", parameters: ["message" : "Firebase integrated"])
         }
         
         tableView.isHidden = true
